6dd8ed5771abe7c88bf3e95ad16a273e56c5d774
kar-21 <karthiks210198@gmail.com>
2022-06-24 20:11:55 +0530

    Add oauth2 and googleapis

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,70 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const env = require("../environment.env");
+const google = require("googleapis");
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/view", (req, res, next) => {
+  res.send("respond with a resource");
 });
+
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Express", redirectURL: urlGoogle() });
+});
+
+const googleConfig = {
+  clientId: env.env.google.clientId,
+  clientSecret: env.env.google.clientSecret,
+  redirect: env.env.google.redirect,
+};
+
+const createConnection = () => {
+  return new google.google.auth.OAuth2(
+    googleConfig.clientId,
+    googleConfig.clientSecret,
+    googleConfig.redirect
+  );
+}
+
+const defaultScope = [
+  "https://www.googleapis.com/auth/plus.me",
+  "https://www.googleapis.com/auth/userinfo.profile",
+  "https://www.googleapis.com/auth/userinfo.email",
+];
+
+const getConnectionUrl = (auth) => {
+  return auth.generateAuthUrl({
+    acces_type: "offline",
+    prompt: "consent",
+    scope: defaultScope,
+  });
+}
+
+const urlGoogle = () => {
+  const auth = createConnection();
+  const url = getConnectionUrl(auth);
+  return url;
+}
+
+const getGoogleAccountFromCode = async(code, response) => {
+  const auth = createConnection();
+
+  const { tokens } = await auth.getToken(code);
+
+  auth.setCredentials(tokens);
+  request(
+    `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${tokens.id_token}`,
+    { json: true },
+    (err, res, body) => {
+      if (err)
+        response
+          .status(500)
+          .send(`some unexpected/uncaught async exception is thrown ${err}`);
+      userData = body;
+      response.redirect("/data");
+    }
+  );
+}
 
 module.exports = router;
