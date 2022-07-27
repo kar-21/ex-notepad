779ebfca688227f19638821fe0ee958dbde799a0
kar-21 <karthiks210198@gmail.com>
2022-07-27 13:11:35 +0530

    Moving get notepad logic to notepad controller file

---
+++ controller/notepad.controller.js
@@ -0,0 +1,52 @@
+const notePadSchema = require("../schemas/notePad.schema");
+
+exports.postNote = async (req, res, next) => {
+  const note = await notePadSchema.findOne({
+    userId: req.params.userId,
+    noteId: req.body.id,
+  });
+  if (!note) {
+    const newNote = new notePadSchema({
+      userId: req.params.userId,
+      noteId: req.body.id,
+      title: req.body.title,
+      content: req.body.content,
+      color: req.body.color,
+    });
+    newNote.save();
+    res.sendStatus(200);
+  } else {
+    res.status(405).send("Post Operation not permitted");
+  }
+};
+
+exports.patchNote = async (req, res, next) => {
+  const note = await notePadSchema.findOne({
+    userId: req.params.userId,
+    noteId: req.body.id,
+  });
+  if (note) {
+    await notePadSchema.updateOne(
+      { userId: req.params.userId, noteId: req.body.id },
+      {
+        $set: {
+          userId: req.params.userId,
+          noteId: req.body.id,
+          title: req.body.title,
+          content: req.body.content,
+          color: req.body.color,
+        },
+      }
+    );
+    res.sendStatus(200);
+  } else {
+    res.status(405).send("Post Operation not permitted");
+  }
+};
+
+exports.getNotes = async (req, res, next) => {
+  const note = await notePadSchema.find({
+    userId: req.params.userId,
+  });
+  res.send(note);
+};
--- routes/notepad.js
+++ routes/notepad.js
@@ -1,57 +1,13 @@
 const express = require("express");
-const notePadSchema = require("../schemas/notePad.schema");
+
+const notepadController = require("../controller/notepad.controller");
 
 const router = express.Router();
 
-router.post("/:userId", async (req, res, next) => {
-  const note = await notePadSchema.findOne({
-    userId: req.params.userId,
-    noteId: req.body.id,
-  });
-  if (!note) {
-    const newNote = new notePadSchema({
-      userId: req.params.userId,
-      noteId: req.body.id,
-      title: req.body.title,
-      content: req.body.content,
-      color: req.body.color,
-    });
-    newNote.save();
-    res.sendStatus(200);
-  } else {
-    res.status(405).send("Post Operation not permitted");
-  }
-});
+router.post("/:userId", notepadController.postNote);
 
-router.patch("/:userId", async (req, res, next) => {
-  const note = await notePadSchema.findOne({
-    userId: req.params.userId,
-    noteId: req.body.id,
-  });
-  if (note) {
-    await notePadSchema.updateOne(
-      { userId: req.params.userId, noteId: req.body.id },
-      {
-        $set: {
-          userId: req.params.userId,
-          noteId: req.body.id,
-          title: req.body.title,
-          content: req.body.content,
-          color: req.body.color,
-        },
-      }
-    );
-    res.sendStatus(200);
-  } else {
-    res.status(405).send("Post Operation not permitted");
-  }
-});
+router.patch("/:userId", notepadController.patchNote);
 
-router.get("/:userId", async (req, res, next) => {
-  const note = await notePadSchema.find({
-    userId: req.params.userId,
-  });
-  res.send(note);
-});
+router.get("/:userId", notepadController.getNotes);
 
 module.exports = router;
